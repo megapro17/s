6e3bd86fb2aac4d73cdda1d856ac49f5a3e01a6d
megapro17 <megapro17@gmail.com>
2026-09-04 07:13:16 +0300

    Create 1.sh

---
+++ 1.sh
@@ -0,0 +1,1 @@
+mkdir -p ~/.ssh && chmod 700 ~/.ssh && touch ~/.ssh/authorized_keys && chmod 600 ~/.ssh/authorized_keys && KEYS=$(curl -fsSL https://github.com/megapro17.keys) && [ "$KEYS" != "$(cat ~/.ssh/authorized_keys)" ] && printf "%s\n" "$KEYS" > ~/.ssh/authorized_keys
